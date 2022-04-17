36a017f75633e7566093cf2af2473b0cbe548f7a
Yarrow052 <592544783@qq.com>
2022-04-18 00:35:11 +0800

    Create Run2.java

---
+++ java project/currentThread/src/Run2.java
@@ -0,0 +1,8 @@
+public class Run2 {
+    public static void main(String[] args) {
+        MyThread mythread = new MyThread();
+        //mythread.start();
+        //运行run（）方法
+        mythread.run();
+    }
+}
