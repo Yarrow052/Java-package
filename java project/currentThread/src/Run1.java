70a1e16adb3533f946246175bd3bf45988acc9d5
Yarrow052 <592544783@qq.com>
2022-04-18 00:35:08 +0800

    Create Run1.java

---
+++ java project/currentThread/src/Run1.java
@@ -0,0 +1,8 @@
+package run;
+
+public class Run1{
+    public static void main(String[] args){
+        //调用currentThread()方法输出当前线程名称
+        System.out.println(Thread.currentThread().getName());
+    }
+}
