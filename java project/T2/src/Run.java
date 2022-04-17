495aad5a405ee10bd055ed03ff3ae7b934b4477c
Yarrow052 <592544783@qq.com>
2022-04-17 19:13:00 +0800

    Create Run.java

---
+++ java project/T2/src/Run.java
@@ -0,0 +1,10 @@
+import myrunnable.MyRunnable;
+
+public class Run {
+    public static void main(String[] args) {
+        Runnable runnable = new MyRunnable();
+        Thread thread = new Thread(runnable);
+        thread.start();
+        System.out.println("运行结束!");
+    }
+}
